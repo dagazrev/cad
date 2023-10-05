aa73f2cb289b65b4dc1982983a1975a4e230845d
Frederik <frederik@asterix.hh.de>
2023-10-05 08:39:59 +0200

    path loader

--- src/utilities.py
+++ src/utilities.py
@@ -1,20 +1,33 @@
 import os
 import glob
+import cv2 
 
 class Utilities:
     def __init__(self):
         pass
     
-    @staticmethod
-    def getImagePathsFromFolders(folder1Path, folder2Path):
-        imagePaths1 = os.listdir(folder1Path + "/nevus")
-        imagePaths2 = os.listdir(folder1Path + "/others")
-        imagePaths3 = os.listdir(folder2Path + "/nevus")
-        imagePaths4 = os.listdir(folder2Path + "/others")
-        imagePaths = [*imagePaths1, *imagePaths2, *imagePaths3, *imagePaths4]
-        return imagePaths
+    def getImagePathsFromFolders(self, trainFolder, valFolder):
+        trainPaths = self.getRelativePaths(trainFolder)
+        valPaths = self.getRelativePaths(valFolder)
+        return trainPaths, valPaths
 
+    @staticmethod
+    def getRelativePaths(folderPath):
+        nevusFolder = os.path.join(folderPath, "nevus")
+        othersFolder = os.path.join(folderPath, "others")
+        nevusPaths = [os.path.join(nevusFolder, file) for file in os.listdir(nevusFolder)]
+        othersPaths = [os.path.join(othersFolder, file) for file in os.listdir(othersFolder)]
+        imagePaths = [*nevusPaths, *othersPaths]
+        return imagePaths
 
     @staticmethod
     def loadImage(imagePath):
-        pass
+        print(imagePath)
+        return cv2.imread(imagePath)
+
+    @staticmethod
+    def getLabel(imagePath):
+        components = imagePath.split(os.sep)
+        label = components[-2]
+        return label
+
