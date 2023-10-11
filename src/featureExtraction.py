0f31f81a1dcf7337f5933b154fde5ee876a444d4
Frederik <frederik@asterix.hh.de>
2023-10-11 18:42:36 +0200

    lbp

--- src/featureExtraction.py
+++ src/featureExtraction.py
@@ -1,4 +1,7 @@
 import cv2 
+import numpy as np
+from skimage import feature
+
 
 class FeatureExtraction:
     def __init__(self):
@@ -7,8 +10,9 @@ class FeatureExtraction:
     def extractFeaturesApproach1(self, image):
         gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 
-        keypointFeatures = self.extractKeypointFeatures(gray)
-        return keypointFeatures
+        #keypointFeatures = self.extractKeypointFeatures(gray)
+        lbpFeatures = self.CompletedRobustLocalBinaryPattern(gray)
+        return lbpFeatures
 
     def extractFeaturesApproach2(self, image):
         pass
@@ -21,6 +25,24 @@ class FeatureExtraction:
 
     def extractSizeFeatures(self, image):
         pass
+    
+    @staticmethod
+    def CompletedRobustLocalBinaryPattern(image, radius=1, neighbors=8):
+        lbp = feature.local_binary_pattern(image, neighbors, radius, method='uniform')
+        crlbp_result = np.zeros((lbp.shape[0]-2, lbp.shape[1]-2))
+
+        for i in range(1, lbp.shape[0]-1):
+            for j in range(1, lbp.shape[1]-1):
+                center = lbp[i, j]
+                neighbors = [lbp[i-1, j-1], lbp[i-1, j], lbp[i-1, j+1], lbp[i, j+1],
+                            lbp[i+1, j+1], lbp[i+1, j], lbp[i+1, j-1], lbp[i, j-1]]
+
+                # Check for the condition of CRLBP
+                if (max(neighbors) - min(neighbors)) <= 2:
+                    crlbp_result[i-1, j-1] = max(neighbors) - min(neighbors)
+
+        return crlbp_result
+
 
     @staticmethod
     def extractKeypointFeatures(image):
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,12 +1,23 @@
+import cv2
+
 class Preprocessing:
     def __init__(self):
         pass
 
     def preprocessApproach1(self, image):
-        return image
+        rescaledImage = self.rescaleImage(image, scalePercent=75)
+        return rescaledImage
 
     def preprocessApproach2(self, image):
         pass
 
     def hairRemoval(self, image):
         pass
+    
+    @staticmethod
+    def rescaleImage(image, scalePercent):
+        width = int(image.shape[1] * scalePercent / 100)
+        height = int(image.shape[0] * scalePercent / 100)
+        dim = (width, height)
+        resized = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+        return resized 
